c75c65160b52f248b64aa8d75a7af0ae3d218cc3
Alvaro Gil <zevarito@gmail.com>
2010-07-01 13:20:08 -0300

    load gems in the correct way

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,14 @@
+begin
+  require File.expand_path('../.bundle/environment', __FILE__)
+rescue LoadError
+  require 'rubygems'
+  require 'bundler'
+  Bundler.setup(:default, :test)
+end
+
 require File.join(File.dirname(__FILE__), "../lib", "mixpanel")
 
-
-MIX_PANEL_TOKEN = "123456789"
+MIX_PANEL_TOKEN = "e2d8b0bea559147844ffab3d607d26a6"
 
 def mixpanel_events_should_include(mixpanel, event, properties)
   mixpanel.events.each do |event_hash|
