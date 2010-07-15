d4963bf87e0bce4049494968f68a06cd2d9d0b03
Alvaro Gil <zevarito@gmail.com>
2010-07-15 17:03:33 -0300

    indent -2 js block #trivial

--- lib/mixpanel/middleware.rb
+++ lib/mixpanel/middleware.rb
@@ -56,20 +56,20 @@ class Middleware
 
   def render_mixpanel_scripts
     <<-EOT
-      <script type='text/javascript'>
-        var mp_protocol = (('https:' == document.location.protocol) ? 'https://' : 'http://');
-        document.write(unescape('%3Cscript src="' + mp_protocol + 'api.mixpanel.com/site_media/js/api/mixpanel.js" type="text/javascript"%3E%3C/script%3E'));
-      </script>
-      <script type='text/javascript'>
-        try {
-          var mpmetrics = new MixpanelLib('#{@token}');
-        } catch(err) {
-          null_fn = function () {};
-          var mpmetrics = {
-            track: null_fn,  track_funnel: null_fn,  register: null_fn,  register_once: null_fn, register_funnel: null_fn
-          };
-        }
-      </script>
+  <script type='text/javascript'>
+    var mp_protocol = (('https:' == document.location.protocol) ? 'https://' : 'http://');
+    document.write(unescape('%3Cscript src="' + mp_protocol + 'api.mixpanel.com/site_media/js/api/mixpanel.js" type="text/javascript"%3E%3C/script%3E'));
+  </script>
+  <script type='text/javascript'>
+    try {
+      var mpmetrics = new MixpanelLib('#{@token}');
+    } catch(err) {
+      null_fn = function () {};
+      var mpmetrics = {
+        track: null_fn,  track_funnel: null_fn,  register: null_fn,  register_once: null_fn, register_funnel: null_fn
+      };
+    }
+  </script>
     EOT
   end
 
