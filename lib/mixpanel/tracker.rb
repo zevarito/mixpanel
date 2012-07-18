fc52375ef73f64c93f38e4b413f46f806e5e25da
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-07-18 17:05:46 +0200

    Added more special_properties
    Source:
    https://mixpanel.com/docs/people-analytics/people-http-specification-ins
    ert-data

--- lib/mixpanel/tracker.rb
+++ lib/mixpanel/tracker.rb
@@ -20,7 +20,7 @@ module Mixpanel
 
     def append_person_event(properties = {})
       # evaluate symbols and rewrite
-      special_properties = %w{email created first_name last_name last_login}
+      special_properties = %w{email created first_name last_name last_login username country_code}
       special_properties.each do |key|
         symbolized_key = key.to_sym
         if properties.has_key?(symbolized_key)
