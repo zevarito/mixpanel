2d98bd24ff17e57a1788b56706aa9942385aa9c0
Alvaro Gil <zevarito@gmail.com>
2012-08-09 12:11:06 -0300

    finally deprecate old way to init mixpanel

--- lib/mixpanel.rb
+++ lib/mixpanel.rb
@@ -1,8 +1,4 @@
 require 'mixpanel/tracker'
 
 module Mixpanel
-  def self.new(token, env, options={})
-    Kernel.warn("DEPRECATED: Use Mixpanel::Tracker.new instead")
-    Mixpanel::Tracker.new(token, env, options)
-  end
 end
--- spec/mixpanel/mixpanel_spec.rb
+++ spec/mixpanel/mixpanel_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 
 describe Mixpanel do
   context "Deprecated initialization mode" do
-    it "should instantiate the object as it was doing before but drop a deprecation warning" do
-      mixpanel = Mixpanel.new(MIX_PANEL_TOKEN, @env = {"REMOTE_ADDR" => "127.0.0.1"})
-      mixpanel.should be_kind_of(Mixpanel::Tracker)
+    it "should not allow to initialize the class as the old way" do
+      lambda do
+        mixpanel = Mixpanel.new(MIX_PANEL_TOKEN, @env = {"REMOTE_ADDR" => "127.0.0.1"})
+      end.should raise_error(NoMethodError)
     end
   end
 end
